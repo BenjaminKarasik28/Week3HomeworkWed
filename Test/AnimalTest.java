66491541ee3700196c8866b2fad3d8fc493a77cf
Ben <karasik.benjamin28@gmail.com>
2019-10-03 09:24:13 -0400

    Update AnimalTest.java

--- Test/AnimalTest.java
+++ Test/AnimalTest.java
@@ -1,16 +1,66 @@
-package GA.homework.Test;
-import GA.homework.Animal;
+package GA.homework.AnimalHW.Test;
+import GA.homework.AnimalHW.Animal;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 public class AnimalTest {
+
     Animal animal = new Animal(4, 31, false, "Lion");
 
     @Test
     public void checkSpeed(){
         assertEquals("The Lion has a speed of: 31 MPH", animal.topSpeedStatement());
     }
+
+
+    @Test
+    public void checkGetEndangered(){
+        assertEquals(false, animal.getIsEndangered());
+    }
+
+    @Test
+    public void checkIsEndangered(){
+        animal.setEndangered(true);
+        assertTrue(animal.getIsEndangered());
+    }
+
+    @Test
+    public void checkGetNumLegs(){
+        assertEquals(4, animal.getNumLegs());
+    }
+
+    @Test
+    public void checkSetNumLegs(){
+        animal.setNumLegs(5);
+        assertEquals(5, animal.getNumLegs());
+    }
+
+    @Test
+    public void checkGetName(){
+        assertEquals("Lion", animal.getName());
+    }
+
+    @Test
+    public void checkSetName(){
+        animal.setName("Pig");
+        assertEquals("Pig", animal.getName());
+    }
+
+    @Test
+    public void checGetTopSpeed(){
+        assertEquals(31, animal.getTopSpeed());
+    }
+
+    @Test
+    public void checkSetTopSpeed(){
+        animal.setTopSpeed(100);
+        assertEquals(100, animal.getTopSpeed());
+    }
+
+
 
 }
 
